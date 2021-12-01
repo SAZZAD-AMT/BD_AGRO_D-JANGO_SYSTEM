761510263a0dbe943a5a3b117f8369899180bbbb
SAZZAD-AMT <80639629+SAZZAD-AMT@users.noreply.github.com>
2021-12-01 22:01:26 +0600

    sazzad 1/12/21

--- statistic/urls.py
+++ statistic/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('statistic/', statistic_view, name="statistic"),
+     path('k18/', k18_view, name="k18"),
 ]
--- statistic/views.py
+++ statistic/views.py
@@ -8,5 +8,10 @@ from user.decorators import unauthenticated_user
 def statistic_view(request):
     return render(request,"statistic.html")
 
+def k18_view(request):
+    return render(request,"k18.html")
+
+
+
 
 
